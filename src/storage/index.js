4cf22bd76c4bd4bc4f4ba218416fb2a907a4a925
doraemon00 <26552699@qq.com>
2020-08-02 14:53:23 +0800

    环境设置

--- src/storage/index.js
+++ src/storage/index.js
@@ -28,6 +28,7 @@ export default {
     let val = this.getStorage();
 
     if (module_name) {
+      if (!val[module_name]) return;
       delete val[module_name][key];
     } else {
       delete val[key];
